a6bcc242aeb32f3606b6300a2507b94973cb1dce
AnastasiiaKalashnikova <nastasyakalashnikova@icloud.com>
2023-10-04 16:49:01 +0200

    1

---
+++ src/сat-api.js
@@ -0,0 +1,22 @@
+export { fetchBreeds, fetchCatByBreed }
+
+const BASE_URL = "https://api.thecatapi.com/v1"
+const KYE_API = "live_dUrCbvWl3gvkPdcDZGQpldZZUJrft6N4eeuhRFreMQE1flDI0IVlKPsxLUR1Pu5X";
+
+
+
+function fetchBreeds() {
+return fetch(`${BASE_URL}/breeds?api_key=${KYE_API}`)
+    .then(response => { return response.json() })
+}
+
+
+
+
+
+
+function fetchCatByBreed(breedId) {
+    return fetch(`${BASE_URL}/images/search?api_key=${KYE_API}&breed_ids=${breedId}`)
+    .then(response => { return response.json() })
+   
+}
---
+++ src/іndex.js
@@ -0,0 +1,41 @@
+const menuSelect = document.querySelector('.breed-select')
+const infoBox = document.querySelector('.cat-info');
+menuSelect.addEventListener("change", handler)
+
+import { fetchBreeds, fetchCatByBreed } from "./сat-api";
+
+
+fetchBreeds().then(cats => {
+    createMarkupOptions(cats);
+    })
+    .catch(error => console.log(error))
+
+
+function handler(evt) {
+    const breedId = menuSelect.value
+fetchCatByBreed(breedId).then(cat => createMarkupCard(cat))
+    .catch(error => console.log(error))
+   
+    //const breedId = evt.currentTarget.value
+    //console.dir(breedId) достукались до ід вибраного кота
+    
+}
+//fetchCatByBreed(breedId)}
+    
+
+
+
+
+//розмітка опцій в селекті
+function createMarkupOptions(array) {
+    const markup = array.map(({ id, name }) => `<option value=" ${id}"> ${name} </option>`).join('');
+        menuSelect.insertAdjacentHTML('beforeend',markup)
+}
+
+//створення розмітки картки вибраного кота
+function createMarkupCard(array) {
+    const markup = array.map(({ url, name, temperament, description}) => `<img src=" ${url}" alt="${name}"> <h2>${name}</h2>
+      <p>${description}</p>
+      <p><b>Temperament:</b>${temperament}</Temperament:b> </p>`).join('');
+    infoBox.insertAdjacentHTML('beforeend',markup)
+}
