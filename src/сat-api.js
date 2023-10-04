3b9ba7c5370155d411e29acc3a78ad4eff0c6d5c
AnastasiiaKalashnikova <nastasyakalashnikova@icloud.com>
2023-10-04 18:36:18 +0200

    Update сat-api.js

--- src/сat-api.js
+++ src/сat-api.js
@@ -1,26 +1,30 @@
-export { fetchBreeds, fetchCatByBreed };
+//export { fetchBreeds, fetchCatByBreed };
 
 const BASE_URL = "https://api.thecatapi.com/v1"
 const KYE_API = "live_dUrCbvWl3gvkPdcDZGQpldZZUJrft6N4eeuhRFreMQE1flDI0IVlKPsxLUR1Pu5X";
 
 
 
-async function fetchBreeds() {
-    const response = await fetch(`${BASE_URL}/breeds?api_key=${KYE_API}`);
-    if (!response.ok) {
-        throw new Error(response.status);
-    }
-    return await response.json();
+export function fetchBreeds() {
+return fetch(`${BASE_URL}/breeds?api_key=${KYE_API}`)
+    .then(response => {
+        if(!response.ok){
+            throw new Error(response.status);
+        }
+        return response.json();
+    })
 }
 
 
 
 
-async function fetchCatByBreed(breedId) {
-    const response = await fetch(`${BASE_URL}/images/search?api_key=${KYE_API}&breed_ids=${breedId}`);
-    if (!response.ok) {
-        throw new Error(response.status);
-    }
-    return await response.json();
+export function fetchCatByBreed(breedId) {
+    return fetch(`${BASE_URL}/images/search?api_key=${KYE_API}&breed_ids=${breedId}`)
+    .then(response => {
+        if(!response.ok){
+            throw new Error(response.status);
+        }
+        return response.json();
+    })
    
 }
